a9b6c330406f4e3d1cc581886a4275b7e679a7a2
Arrodl <arrodl40@gmail.com>
2018-10-23 20:11:21 -0500

    creating schedule and relationshisp model

--- api/models.py
+++ api/models.py
@@ -20,9 +20,28 @@ class DayOfWeek (models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
-    # Relationships
-    common_area = models.ForeignKey(CommonArea, on_delete = models.CASCADE)
-
     def __str__(self):
         return "%s" % (self.identifier)
+
+class CommonAreaDay (models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    # Relationships
+    common_area = models.ForeignKey(CommonArea, on_delete = models.CASCADE)
+    day_of_week = models.ForeignKey(DayOfWeek, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "Se utiliza para relacionar el area comun: <%s> y el día: <%s>" % (self.common_area.name, self.day_of_week.identifier)
+
+class Schedule (models.Model):
+    start = models.TimeField()
+    end = models.TimeField()
+
+    # Relationships
+    common_area_day = models.ForeignKey(CommonAreaDay, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "%s - %s" % (self.start, self.end)
+
 
