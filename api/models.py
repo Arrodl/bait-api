dec000f59867c90da73787fec47d024c80fc00a5
Arrodl <arrodl40@gmail.com>
2018-10-24 22:33:36 -0500

    Still cannot deploy to heroku

--- api/models.py
+++ api/models.py
@@ -44,10 +44,12 @@ class Schedule (models.Model):
     def __str__(self):
         return "%s - %s" % (self.start, self.end)
 
-class Announcements (models.Model):
-    
+class Announcement (models.Model):
+    title = models.CharField(max_length = 100)
+    body = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return ""
-
-
